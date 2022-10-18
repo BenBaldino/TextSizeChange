69192957d1224bdfdde2b8b51587f896c7e9d527
Ben Baldino <tud13556@temple.edu>
2022-10-18 14:49:02 -0400

    started, but did not understand enough to make real progress.

---
+++ app/src/main/java/edu/temple/test/ViewModel.kt
@@ -0,0 +1,12 @@
+package edu.temple.test
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.MutableLiveData
+
+class ViewModel : ViewModel() {
+
+    var text = MutableLiveData<Any>()
+
+    fun
+
+}
